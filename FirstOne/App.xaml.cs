6072260207bdcb55e78d8d9fc8c290626e474bfb
Shalin Dileesha <135975902+dileesha-shalin@users.noreply.github.com>
2024-03-15 11:17:18 +0530

    Add files via upload

---
+++ FirstOne/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FirstOne
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
